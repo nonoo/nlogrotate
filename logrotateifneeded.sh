8a16d681176a4841f1de37fb4992d83afb5d6cb0
Nonoo <nonoo@nonoo.hu>
2013-10-02 16:41:05 +0200

    initial commit, added files

---
+++ logrotateifneeded.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+scriptname=`basename $0`
+scriptdir=${0/$scriptname/}
+logfile=$scriptdir/logrotate.log
+
+source $scriptdir/config
+source $scriptdir/redirectlog.src.sh
+
+lf=$1
+
+if [ ! -f "$lf" ]; then
+	echo "file \"$lf\" doesn't exist."
+	exit 1
+fi
+
+if [ `du $lf | awk '{print $1}'` -gt $maxlogsizeinkb ]; then
+	echo "logfile \"$lf\" is over size, needs rotating."
+	$scriptdir/logrotate.sh $lf
+fi
