c640821fa1b47896514b29a3dc823a6df62abbcd
agron-fixmero3 <agron-fixmero3@bme-infokom.hu>
2014-01-29 15:31:27 +0000

    self logging removed

--- logrotateifneeded.sh
+++ logrotateifneeded.sh
@@ -2,18 +2,19 @@
 
 scriptname=`basename $0`
 scriptdir=${0/$scriptname/}
-logfile=$scriptdir/logrotate.log
 
 source $scriptdir/config
-source $scriptdir/redirectlog.src.sh
 
 lf=$1
 
 if [ ! -f "$lf" ]; then
-	exit 1
+	exit 2
 fi
 
 if [ `du $lf | awk '{print $1}'` -gt $maxlogsizeinkb ]; then
 	echo "logfile \"$lf\" is over size, needs rotating."
 	$scriptdir/logrotate.sh $lf
+	exit 0
 fi
+
+exit 1
