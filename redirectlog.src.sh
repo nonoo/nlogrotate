50e359702eab97241274314e5c8f2d6c64171832
Nonoo <nonoo@nonoo.hu>
2014-01-30 11:44:46 +0100

    moved redirectlog stuff to a function, and added a $quietmode variable which is checked when the functions are called.

--- redirectlog.src.sh
+++ redirectlog.src.sh
@@ -11,29 +11,52 @@ if [ -z "$logfile" ]; then
 	exit 1
 fi
 
-# Creating the directory for the logfile if it doesn't exist
-logdir=`dirname $logfile`
-if [ ! -d $logdir ]; then
-	mkdir -p $logdir
-	chmod -f o+w $logdir
-fi
+checklogsize() {
+	if [ $quietmode -ne 1 ]; then
+		return
+	fi
 
-logpipe=/tmp/$scriptname.pipe
-if [ -e $logpipe.pid ]; then
-	kill -9 `cat $logpipe.pid` &>/dev/null
-fi
-rm -f $logpipe*
-# Creating pipe
-mknod $logpipe p
-# Reading from the log pipe and processing it.
-awk '{ print strftime("[%Y/%m/%d %H:%M:%S]"), $0; }' $logpipe >> $logfile &
-awkpid=$!
-echo $awkpid > $logpipe.pid
-# Setting up a trap to delete the pipe on exit
-trap "kill -9 $awkpid" INT TERM EXIT
-trap "rm -f $logpipe*" INT TERM EXIT
-# Closing stdout
-exec 1>&-
-# Redirecting stdout to the pipe
-exec 1>$logpipe
-exec 2>&1
+	$logrotateifneeded $logfile
+	# If logrotate happened, the exit code of the script is 0.
+	if [ $? -eq 0 ]; then
+		redirectlog
+	fi
+}
+
+redirectlog() {
+	if [ $quietmode -ne 1 ]; then
+		return
+	fi
+
+	# Creating the directory for the logfile if it doesn't exist
+	logdir=`dirname $logfile`
+	if [ ! -d $logdir ]; then
+		mkdir -p $logdir
+		chmod -f o+w $logdir
+	fi
+
+	logpipe=/tmp/$scriptname.pipe
+	if [ -e $logpipe.pid ]; then
+		kill -9 `cat $logpipe.pid` &>/dev/null
+	fi
+	rm -f $logpipe*
+
+	# Creating pipe
+	mknod $logpipe p
+
+	# Reading from the log pipe and processing it.
+	awk '{ print strftime("[%Y/%m/%d %H:%M:%S]"), $0; }' $logpipe >> $logfile &
+	awkpid=$!
+	echo $awkpid > $logpipe.pid
+
+	# Setting up a trap to delete the pipe on exit
+	trap "kill -9 $awkpid" INT TERM EXIT
+	trap "rm -f $logpipe*" INT TERM EXIT
+
+	# Closing stdout
+	exec 1>&-
+
+	# Redirecting stdout to the pipe
+	exec 1>$logpipe
+	exec 2>&1
+}
